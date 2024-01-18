a3b9827ebaf30d952f58e0655bd1644936cfd79e
Yuriy Znatokov <hey@devopsbarista.com>
2024-01-18 11:58:33 +0200

    boilerplate setup

---
+++ next.config.mjs
@@ -0,0 +1,64 @@
+// import { withContentlayer } from 'next-contentlayer'
+// import { withPlausibleProxy } from 'next-plausible'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
+
+const csp = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self' data:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: csp.replace(/\n/g, ''),
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+]
+
+export default nextConfig
+// export default withContentlayer(withPlausibleProxy()(nextConfig))
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
 import type { Config } from 'tailwindcss'
+import animatePlugin from 'tailwindcss-animate'
+import ariaPlugin from 'tailwindcss-react-aria-components'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      sans: ['var(--font-geist-sans)', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      mono: ['var(--font-geist-mono)', 'ui-monospace', 'monospace'],
     },
+    extend: {},
   },
-  plugins: [],
+  plugins: [animatePlugin, ariaPlugin],
 }
 export default config
